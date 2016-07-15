ba5b65d254690580457bb8399e04457d5e25bff4
Josh English <josh@joshenglish.com>
2016-07-15 08:19:29 -0500

    Changing the Voting component to Vote and calling winner from the Voting component

---
+++ src/components/Vote.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class extends Component {
+  getPair() {
+    return this.props.pair || []
+  }
+  isDisabled() {
+    return !!this.props.hasVoted
+  }
+  hasVotedFor() {
+    return this.props.hasVoted === entry
+  }
+  render() {
+    return (
+      <div className="voting">
+        {this.props.winner ?
+          <div ref="winner">Winner is {this.props.winner}!</div> :
+          this.getPair().map(entry =>
+            <button key={entry}
+                    disabled={this.isDisabled()}
+                    onClick="{() => this.props.vote(entry)">
+              <h1>{entry}</h1>
+              {this.hasVotedFor(entry) ? <div className="label">Voted</div> : null}
+            </button>
+          )
+        }
+      </div>
+    )
+  }
+}
--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Winner extends Component {
+export default class extends Component {
   render() {
     return (
       <div className="winner">
