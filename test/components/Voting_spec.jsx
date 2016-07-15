bcc953506f82b3a0b96edbfa29b04742e9ce17fa
Josh English <josh@joshenglish.com>
2016-07-15 14:03:24 -0500

    Adding the test for testing 'updates on DOM when prop changes'

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -6,6 +6,7 @@ import {
     Simulate
 } from 'react-addons-test-utils'
 
+import { List } from 'immutable'
 import Voting from '../../src/components/Voting'
 import { expect } from 'chai'
 
@@ -84,4 +85,24 @@ describe ('Voting', () => {
         firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
         expect(firstButton.textContent)to.equal('Trainspotting')
     })
+    it('does update DOM when prop changes', () => {
+        const pair = List.of('Trainspotting', '28 Days Later')
+        const container = document.createElement('div')
+        let component = ReactDOM.render(
+          <Voting pair={pair} />,
+          container
+        )
+
+        let firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+        expect(firstButton.textContent).to.equal('Trainspotting')
+
+        const newPair = pair.set(0, 'Sunshine')
+        component = ReactDOM.render(
+          <Voting pair={newPair} />,
+          component
+        )
+        firstButton = scryRenderedDOMComponentsWithTag(component, 'button')[0]
+        expect(firstButton.textContent)to.equal('Sunshine')
+      }
+    )
 })
