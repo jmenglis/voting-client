c2087dd5c98e9e0a2f98d54934aea21b362024ef
Josh English <josh@joshenglish.com>
2016-07-18 12:24:08 -0500

    Adding the winner to the Results page

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import Winner from './Winner'
 
 export default class extends Component {
   constructor(props) {
@@ -15,14 +16,14 @@ export default class extends Component {
     }
     return 0
   }
-
   render() {
-    return (
+    return ( this.props.winner ?
+      <Winner ref="winner" winner={this.props.winner} /> :
       <div className="results">
         <div className="tally">
           {this.getPair().map(entry =>
             <div key={entry} className="entry">
-              <h1>Entry</h1>
+              <h1>{entry}</h1>
               <div className="voteCount">
                 {this.getVotes(entry)}
               </div>
