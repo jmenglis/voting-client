8143990289c9f9ed47af181a396af87daa3d1f58
Josh English <josh@joshenglish.com>
2016-07-18 13:15:35 -0500

    Adding redux-store to voting component

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { connect } from 'react-redux'
 import Winner from './Winner'
 import Vote from './Vote'
 
-export default class extends Component {
+export default class Voting extends Component {
   constructor(props) {
     super(props)
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
@@ -18,3 +19,12 @@ export default class extends Component {
     )
   }
 }
+
+var mapStateToProps = (state) => {
+  return {
+    pair: state.getIn(['vote', 'pair']),
+    winner: state.get('winner')
+  }
+}
+
+connect(mapStateToProps)(Voting);
