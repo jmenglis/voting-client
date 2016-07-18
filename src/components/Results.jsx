61b6a77ff89f9cd42db423493598ef78afb73263
Josh English <josh@joshenglish.com>
2016-07-18 12:03:25 -0500

    Adjusted the results page to receive data from the App

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -6,7 +6,28 @@ export default class extends Component {
     super(props)
     this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this)
   }
+  getPair() {
+    return this.props.pair || []
+  }
+  getVotes() {
+    if (this.props.tally && this.props.tally.has(entry)) {
+      return this.props.tally.get(entry)
+    }
+    return 0
+  }
+
   render() {
-    return <div>Hello from results</div>
+    return (
+      <div className="results">
+        {this.getPair().map(entry =>
+          <div key={entry} className="entry">
+            <h1>Entry</h1>
+            <div className="voteCount">
+              {this.getVotes(entry)}
+            </div>
+          </div>
+        )}
+      </div>
+    )
   }
 }
