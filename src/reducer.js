630368b78ff4ac711da4eb4a03db22b9fd2a9feb
Josh English <josh@joshenglish.com>
2016-07-18 20:31:32 -0500

    Adding the vote function to the reducer

--- src/reducer.js
+++ src/reducer.js
@@ -4,10 +4,21 @@ var setState = (state, newState) => {
   return state.merge(newState)
 }
 
+var vote = (state, entry) => {
+  const currentPair = state.getIn(['vote', 'pair'])
+  if (currentPair && currentPair.includes(entry)) {
+    return state.set('hasVoted', entry)
+  } else {
+    return state
+  }
+}
+
 export default (state = Map(), action) => {
   switch (action.type) {
     case 'SET_STATE':
       return setState(state, action.state)
+    case 'VOTE':
+      return vote(state, action.entry)
   }
   return state
 }
--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -85,5 +85,5 @@ describe('reducer', () => {
       }
     }))
   })
-  
+
 })
